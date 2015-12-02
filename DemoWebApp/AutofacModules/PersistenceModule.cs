7f4f63b6142aff3630fb7fd1ec8feda632bd0183
Andrew Harcourt <andrewh@uglybugger.org>
2015-12-02 17:20:32 +1000

    Update-Package all the things

---
+++ DemoWebApp/AutofacModules/PersistenceModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using DemoWebApp.Core;
+
+namespace DemoWebApp.AutofacModules
+{
+    public class PersistenceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterGeneric(typeof (MemoryRepository<>))
+                .As(typeof (IRepository<>))
+                .SingleInstance();
+        }
+    }
+}
